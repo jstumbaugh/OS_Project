e923febc9c6f396d1ffcbd61effb8bca23852372
jstumbaugh <jstumbaugh@smu.edu>
2016-03-28 20:18:32 -0500

    added interactive part

--- main.cpp
+++ main.cpp
@@ -13,6 +13,27 @@
 
 using namespace std;
 
+// Prompt user for process information
+PCB enter_process_info(string state="0") {
+    int pid, priority, job_time;
+    string program_counter;
+    cout << "Process ID: ";
+    cin >> pid;
+    cout << "Priority: ";
+    cin >> priority;
+    if (state == "0") {
+        cout << "State (ready, waiting, running, terminated): ";
+        cin >> state;
+    }
+    cout << "Program Counter Address: ";
+    cin >> program_counter;
+    cout << "Job Time: ";
+    cin >> job_time;
+    PCB pcb(pid, priority, state, program_counter, job_time);
+    return pcb;
+}
+
+// The main worked method of the operating system
 int main() {
     cout << "CSE 7343: Operating Systems Project\nJason Stumbaugh\n";
     int mode = 0;
@@ -27,26 +48,10 @@ int main() {
 
     if (mode == 1) { // Manually Enter Processes
         cout << "\nManual Mode selected. Please enter in the processes:\n";
-        int pid, priority, job_time;
-        string state, program_counter;
+
         char entering_processes = 'y';
         while (entering_processes == 'y') {
-            state = "";
-            // read in information about Process
-            cout << "Process ID: ";
-            cin >> pid;
-            cout << "Priority: ";
-            cin >> priority;
-            while (state != "ready" && state != "waiting" && state != "running" && state != "terminated") {
-                cout << "State (ready, waiting, running, terminated): ";
-                cin >> state;
-            }
-            cout << "Program Counter: ";
-            cin >> program_counter;
-            cout << "Job Time: ";
-            cin >> job_time;
-            PCB pcb(pid, priority, state, program_counter, job_time);
-
+            PCB pcb = enter_process_info();
             // move PCB into correct queue
             if (pcb.get_state() == "ready") {
                 ready.push_back(pcb);
@@ -77,13 +82,17 @@ int main() {
                     continue;
                 } else {
                     stringstream ss(line);
-                    int pid, priority;
+                    int pid, priority, job_time;
                     string state, program_counter;
                     ss >> pid;
                     ss >> priority;
                     ss >> state;
                     ss >> program_counter;
-                    PCB pcb(pid, priority, state, program_counter);
+                    if (ss >> job_time)
+                        cout << "";
+                    else
+                        job_time = 0;
+                    PCB pcb(pid, priority, state, program_counter, job_time);
                     // move PCB into correct queue
                     if (pcb.get_state() == "ready") {
                         ready.push_back(pcb);
@@ -96,6 +105,9 @@ int main() {
                     }
                 }
             }
+        } else {
+            cerr << "Error opening " << file_name << endl;
+            exit(1);
         }
     }
 
@@ -105,12 +117,73 @@ int main() {
     Queue running_queue("Running", running);
     Queue terminated_queue("terminated", terminated);
 
-    double sjf_wait = sjf_scheduler(ready_queue,waiting_queue);
-    cout << "Shortest Job First Scheduler average wait time:   " << sjf_wait << endl;
-    double pri_wait = priority_scheduler(ready_queue, waiting_queue);
-    cout << "Priority Scheduler average wait time:             " << pri_wait << endl;
-    double rr_wait = rr_scheduler(ready_queue, waiting_queue, 4);
-    cout << "Round Robin Scheduler average wait time:          " << rr_wait << endl;
+    bool using_os = true;
+    while (using_os) {
+        cout << "\nQueues created. Please select an option:\n";
+        cout << "   1  - Print contents of Ready Queue.\n";
+        cout << "   2  - Print contents of Waiting Queue.\n";
+        // cout << "   3  - Print contents of Running Queue.\n";
+        // cout << "   4  - Print contents of Terminated Queue.\n";
+        cout << "   3  - Run the Shortest Job First Scheduler on the Ready queue.\n";
+        cout << "   4  - Run the Priority Scheduler on the Ready queue.\n";
+        cout << "   5  - Run the Round Robin Scheduler on the Ready queue.\n";
+        cout << "   6  - Run all of the schedulers on the Ready queue.\n";
+        cout << "   7  - Add PCB to a given queue.\n";
+        cout << "   0  - Exit Operating System.\n";
+
+        int input;
+        cout << "> ";
+        cin >> input;
+        cout << endl;
+        int q;
+        string queue_to_add_to;
+        PCB pcb;
+        switch(input) {
+            case 1 :    ready_queue.print();
+                        break;
+            case 2 :    waiting_queue.print();
+                        break;
+            case 3 :    cout << "Shortest Job First Scheduler average wait time: " << sjf_scheduler(ready_queue,waiting_queue) << endl;
+                        break;
+            case 4 :    cout << "Priority Scheduler average wait time: " << priority_scheduler(ready_queue, waiting_queue) << endl;
+                        break;
+            case 5 :    cout << "Please enter the Time Quantum to use: ";
+                        cin >> q;
+                        cout << "\nRound Robin Scheduler average wait time: " << rr_scheduler(ready_queue, waiting_queue, q) << endl;
+                        break;
+            case 6 :    cout << "Please enter the Time Quantum to use: ";
+                        cin >> q;
+                        cout << "\nShortest Job First Scheduler average wait time:    " << sjf_scheduler(ready_queue,waiting_queue) << endl;
+                        cout << "Priority Scheduler average wait time:              " << priority_scheduler(ready_queue, waiting_queue) << endl;
+                        cout << "Round Robin Scheduler average wait time:           " << rr_scheduler(ready_queue, waiting_queue, q) << endl;
+                        break;
+            case 7 :    cout << "Add PCB to which queue? (ready, waiting): ";
+                        cin >> queue_to_add_to;
+                        if (queue_to_add_to == "ready") {
+                            pcb = enter_process_info("ready");
+                            cout << "Please enter the position you would like to add the PCB in the ready queue: ";
+                            cin >> q;
+                            ready_queue.push(pcb,q);
+                            cout << "PCB added to the Ready queue.\n";
+                            break;
+                        } else if (queue_to_add_to == "waiting") {
+                            pcb = enter_process_info("waiting");
+                            cout << "Please enter the position you would like to add the PCB in the waiting queue: ";
+                            cin >> q;
+                            waiting_queue.push(pcb,q);
+                            cout << "PCB added to the Waiting queue.\n";
+                            break;
+                        } else {
+                            cout << "Please enter in a vaild queue name (ready, waiting)";
+                            break;
+                        }
+                        break;
+            case 0 :    using_os = false;
+                        break;
+            default :   cout << "Please enter in a valid command.\n";
+                        break;
+        }
+    }
 
     return 0;
 }
